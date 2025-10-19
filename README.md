dcdae957a69b7df5cca4654bd13c56dfc2c8074a
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2025-10-19 14:11:31 +0200

    Mise à jour du README et du code principal : ajout de la description de la simulation et des vecteurs 3D, ainsi que d'un fichier .gitignore.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Aash-Simulator
 Simulateur 3d du robot hexapode Aash ( en Rust )
+
+La simulation est sous forme de vecteurs pour le moment et le logiciel est en cours de développement étant donnée que j'apprends le Rust avec ce petit simulateur, il peut mettre un certain temps avant d'être terminé, merci de votre compréhension !
--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,26 @@ async fn main() {
     let mut window = Window::new("Simulation vectorielle 3D - Kiss3d");
 
     // Caméra orbitale (autour du point (0, 0, 0))
-    let eye = Point3::new(30.0, 30.0, 30.0);
+    let eye = Point3::new(20.0, 20.0, 20.0);
     let at = Point3::origin();
     let mut camera = ArcBall::new(eye, at);
 
-    // Définition de deux vecteurs 3D
-    let start1 = Point3::new(0.0, 0.0, 0.0);
-    let end1   = Point3::new(3.0, 1.0, 0.0);
+    // Définition de deux vecteurs 3D & dimensions
+    let coxa_length = 5.0;   // Longueur du coxa en cm
+    let femur_length = 6.3;  // Longueur du femur en cm
+    let tibia_length = 13.0; // Longueur du tibia en cm
 
+    // Coxa: vecteur 1 | 5cm
+    let start1 = Point3::new(0.0, 0.0, 0.0);
+    let end1   = Point3::new(coxa_length, 0.0, 0.0);
+
+    // Femur: vecteur 2 | 6.3cm
     let start2 = end1;
-    let end2   = Point3::new(6.0, 1.0, 0.0);
+    let end2   = Point3::new(coxa_length + femur_length, 0.0, 0.0);
+
+    // Tibia: vecteur 3 | 13cm
+    let start3 = end2;
+    let end3 = Point3::new(coxa_length + femur_length + tibia_length, 0.0, 1.0);
 
     // Boucle principale
     while window.render_with_camera(&mut camera).await {
@@ -31,6 +41,9 @@ async fn main() {
 
         // Ligne reliant les deux vecteurs (rouge)
         window.draw_line(&end1, &end2, &Point3::new(1.0, 0.0, 0.0));
+
+        // Dessin du troisième vecteur (jaune)
+        window.draw_line(&start3, &end3, &Point3::new(1.0, 1.0, 0.0));
 
         // Axes de repère
         window.draw_line(&Point3::origin(), &Point3::new(1.0, 0.0, 0.0), &Point3::new(0.5, 0.5, 0.5));
