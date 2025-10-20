eab535ec9c030cb38b75a57a186a3b2593a30b6c
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2025-10-20 19:00:01 +0200

    Amélioration du message d'affichage dans le test de la patte pour une meilleure lisibilité.

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,7 +1,7 @@
 use aash_simulator::robot::parts::{Leg, LegPosition};
 
 fn main() {
-    println!("Instance de test pour la conception d'une patte et la cinématique inverse.");
+    println!("===== Instance de test pour la conception d'une patte et la cinématique inverse. =====\n");
 
     let mut leg = Leg::new(1, 10.0, 20.0, 30.0);
     LegPosition::set_position(&mut leg, 10.0, 10.0, 15.0);
