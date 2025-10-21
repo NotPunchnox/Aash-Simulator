9f0ce2e3beab6c73ca3001ab63fa4d799edf40a9
NotPunchnox <punchnoxpro@gmail.com>
2025-10-21 12:18:00 +0200

    Liaison entre main.rs et les classes LEG/POSITION

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,6 +4,8 @@ use kiss3d::camera::ArcBall;
 use kiss3d::nalgebra::{Point3};
 use kiss3d::window::Window;
 
+use aash_simulator::robot::parts::{Leg, LegPosition};
+
 
 #[kiss3d::main]
 async fn main() {
@@ -12,39 +14,37 @@ async fn main() {
 
     // Caméra orbitale (autour du point (0, 0, 0))
     let eye = Point3::new(20.0, 20.0, 20.0);
-    let at = Point3::origin();
+    let at  = Point3::origin();
     let mut camera = ArcBall::new(eye, at);
 
-    // Définition de deux vecteurs 3D & dimensions
-    let coxa_length = 5.0;   // Longueur du coxa en cm
-    let femur_length = 6.3;  // Longueur du femur en cm
-    let tibia_length = 13.0; // Longueur du tibia en cm
+    // Instanciation d'une patte;
+    let leg  = Leg::new(1, 5.0, 6.3, 13.0);
 
     // Coxa: vecteur 1 | 5cm
-    let start1 = Point3::new(0.0, 0.0, 0.0);
-    let end1   = Point3::new(coxa_length, 1.0, 0.0);
+    let c1   = Point3::new(0.0, 0.0, 0.0);
+    let c2   = Point3::new(leg.get_coxa_length(), 1.0, 0.0);
 
     // Femur: vecteur 2 | 6.3cm
-    let start2 = end1;
-    let end2   = Point3::new(coxa_length + femur_length, 0.0, 0.0);
+    let f1   = c2;
+    let f2   = Point3::new(leg.get_coxa_length() + leg.get_femur_length(), 0.0, 0.0);
 
     // Tibia: vecteur 3 | 13cm
-    let start3 = end2;
-    let end3 = Point3::new(coxa_length + femur_length + tibia_length, 0.0, 0.0);
+    let t1 = f2;
+    let t2 = Point3::new(leg.get_coxa_length() + leg.get_femur_length() + leg.get_tibia_length(), 0.0, 0.0);
 
     // Boucle principale
     while window.render_with_camera(&mut camera).await {
         // Dessin du premier vecteur (vert)
-        window.draw_line(&start1, &end1, &Point3::new(0.0, 1.0, 0.0));
+        window.draw_line(&c1, &c2, &Point3::new(0.0, 1.0, 0.0));
 
         // Dessin du second vecteur (bleu)
-        window.draw_line(&start2, &end2, &Point3::new(0.0, 0.0, 1.0));
+        window.draw_line(&f1, &f2, &Point3::new(0.0, 0.0, 1.0));
 
         // Ligne reliant les deux vecteurs (rouge)
-        window.draw_line(&end1, &end2, &Point3::new(1.0, 0.0, 0.0));
+        window.draw_line(&c2, &f2, &Point3::new(1.0, 0.0, 0.0));
 
         // Dessin du troisième vecteur (jaune)
-        window.draw_line(&start3, &end3, &Point3::new(1.0, 1.0, 0.0));
+        window.draw_line(&t1, &t2, &Point3::new(1.0, 1.0, 0.0));
 
         // Axes de repère
         window.draw_line(&Point3::origin(), &Point3::new(1.0, 0.0, 0.0), &Point3::new(0.5, 0.5, 0.5));
--- src/robot/utils/math.rs
+++ src/robot/utils/math.rs
@@ -3,6 +3,10 @@ pub fn radians_to_degrees(rad: f32) -> f32 {
     return rad * (180.0 / std::f32::consts::PI);
 }
 
+pub fn degrees_to_radians(deg: f32) -> f32 {
+    return deg * (std::f32::consts::PI / 180.0);
+}
+
 pub fn al_kashi(a: f32, b: f32, c: f32) -> f32 {
     let numerator = b.powi(2) + c.powi(2) - a.powi(2);
     let denominator = 2.0 * b * c;
